45572d10f420da3cfd5016157ee81c1810e7fa2e
Default Seadas User <seadas-user@localhost>
2015-11-25 17:10:04 -0800

    these, too.

---
+++ Umbra/umbra_common.py
@@ -0,0 +1,5 @@
+
+ug_formats=[ dict(name='SUNTANS',long_name='SUNTANS {cells,edges,points}.dat',is_dir=True),
+             dict(name='SMS',long_name='SMS     *.grd',is_dir=False),
+             dict(name='DFM',long_name='DFlow FM *_net.nc',is_dir=False),
+             dict(name='pickle',long_name='UnstructuredGrid pickle',is_dir=False) ]
---
+++ Umbra/umbra_savelayer.py
@@ -0,0 +1,99 @@
+"""
+/***************************************************************************
+ UmbraSaveLayer
+                                 A QGIS plugin
+ Unstructured mesh builder
+                             -------------------
+        begin                : 2015-11-10
+        git sha              : $Format:%H$
+        copyright            : (C) 2015 by Rusty Holleman
+        email                : rustychris@gmail.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
+
+import os
+
+from PyQt4 import QtGui, uic
+from PyQt4.QtCore import pyqtSignal
+
+FORM_CLASS, base_class = uic.loadUiType(os.path.join(
+    os.path.dirname(__file__), 'umbra_savelayer_base.ui'))
+
+import umbra_common
+
+class UmbraSaveLayer(base_class, FORM_CLASS):
+
+    closingPlugin = pyqtSignal()
+
+    def __init__(self, parent=None, iface=None):
+        """Constructor."""
+        super(UmbraSaveLayer, self).__init__(parent)
+        # Set up the user interface from Designer.
+        # After setupUI you can access any designer object by doing
+        # self.<objectname>, and you can use autoconnect slots - see
+        # http://qt-project.org/doc/qt-4.8/designer-using-a-ui-file.html
+        # #widgets-and-dialogs-with-auto-connect
+        self.iface=iface
+
+        self.setupUi(self)
+
+        for fmt in umbra_common.ug_formats:
+            self.formatCombo.addItem(fmt['long_name'])
+        
+        self.browseButton.clicked.connect(self.on_browse)
+        self.buttonBox.accepted.connect(self.on_ok_clicked)
+        self.buttonBox.rejected.connect(self.on_cancel_clicked)
+
+    def closeEvent(self, event):
+        self.closingPlugin.emit()
+        event.accept()
+
+    def on_browse(self):
+        fmt=self.fmt()
+
+        if fmt['is_dir']:
+            path=QtGui.QFileDialog.getExistingDirectory(self,'Folder for %s'%fmt['name'],
+                                                        os.environ['HOME'])
+        else:
+            path=QtGui.QFileDialog.getOpenFileName(self, 'Filename for %s'%fmt['name'], 
+                                                   os.environ['HOME'])
+        print "Filename ",path
+        if path is not None:
+            self.lineEdit.setText( path )
+        return True 
+        
+    def fmt(self): # should be abstracted to common class
+        sel=self.formatCombo.currentText()
+
+        for fmt in umbra_common.ug_formats:
+            if sel==fmt['long_name']:
+                return fmt
+
+        assert False
+        
+    def on_ok_clicked(self):
+        path=self.lineEdit.text()
+        fmt=self.fmt()
+        print "Would be saving to ",path
+
+        clayer = self.iface.mapCanvas().currentLayer()
+        grid=clayer.grid
+
+        if fmt['name']=='SUNTANS':
+            grid.write_suntans(path)
+        elif fmt['name']=='pickle':
+            grid.write_pickle(path)
+
+    def on_cancel_clicked(self):
+        pass
+
+
